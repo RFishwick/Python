3e086c12768b9c7667fa76f96641c145ed10fdc5
Randy Fishwick <rfishwick@hotmail.com>
2017-11-11 22:00:56 -0800

    cosmetic variable name changes

--- FindWords.py
+++ FindWords.py
@@ -5,12 +5,12 @@ import WordFinder
 # Begin Main Program
 
 fileI = open('theMessage.txt', 'r')  # Open Input file for reading
-myText = list(fileI)  # Read file into string myText
+my_text = list(fileI)  # Read file into string myText
 fileI.close()  # Close input file
 
 # Print lines that contain the selected word
 lineCnt = 0
-for line in myText:
+for line in my_text:
     lineCnt += 1
     theResult = WordFinder.findWord(line)
     if theResult != None: # Word Found
