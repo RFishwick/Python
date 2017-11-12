af467ec1561cceb70e2a0d36cc953fdb58fcf425
Randy Fishwick <rfishwick@hotmail.com>
2017-11-11 21:56:14 -0800

    Changes made with VS Code Studio

--- WordFinder.py
+++ WordFinder.py
@@ -1,4 +1,4 @@
-def findWord(iLine):  # Function to find a word within a string
+def findWord(i_line):  # Function to find a word within a string
     """Return first word in sentence matching a word in the searchWords tuple"""
 
     # Define the words to search for the tuple searchWords
@@ -6,16 +6,18 @@ def findWord(iLine):  # Function to find a word within a string
 
     result = None  # Pre-set result to None in case loop falls through
     try:
-        iLine = iLine.lower()  # Convert string to lower case for searching
+        i_line = i_line.lower()  # Convert string to lower case for searching
 
         # Loop through words in seachWords and try to find in string s
         for searchWord in searchWords:
-            position = iLine.find(searchWord)  # Get postion of word in iLine
+            position = i_line.find(searchWord)  # Get postion of word in i_line
+
             if position != -1:
                 result = searchWord  # Set result to value of found searchWord
                 break
 
-        # None will be returned if no search words found in string iLine
+        # None will be returned if no search words found in string i_line
+
         return result
 
     except:
