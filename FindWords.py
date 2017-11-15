c8cab41d19f9739156c1221ca064fd6f275935d9
Randy Fishwick <rfishwick@hotmail.com>
2017-11-14 21:58:32 -0800

    Correct syntax for use of None value

--- FindWords.py
+++ FindWords.py
@@ -4,14 +4,16 @@ import WordFinder
 
 # Begin Main Program
 
-fileI = open('theMessage.txt', 'r')  # Open Input file for reading
-my_text = list(fileI)  # Read file into string myText
-fileI.close()  # Close input file
+file_i = open('theMessage.txt', 'r')  # Open Input file for reading
+my_text = list(file_i)  # Read file into string myText
+file_i.close()  # Close input file
+
 
 # Print lines that contain the selected word
 lineCnt = 0
 for line in my_text:
     lineCnt += 1
-    theResult = WordFinder.find_word(line)
-    if theResult != None: # Word Found
-        output = '\'{}\' found on line {}:\n{}\n'.format(theResult.title(), lineCnt, line.strip())
+    the_result = WordFinder.find_word(line)
+    if the_result is not None:  # Word Found
+        output = "'{}' found on line {}:\n{}\n".format(the_result.title(), lineCnt, line.strip())
+        print(output)
